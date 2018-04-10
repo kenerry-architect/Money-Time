6249d08859d845175c429138e6c659a7711cd6a2
Kenerry Pierry Paulo Serain <kserain.development@gmail.com>
2018-04-10 18:57:58 -0300

    Starting Identity Auth

---
+++ src/Application/MoneyTime.Application.WebApi.Scheduling/Settings/SchedulingSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace MoneyTime.Application.WebApi.Settings
+{
+    public class SchedulingSettings
+    {
+        public string IdentityUrl { get; set; }
+        public string IdentityAudience { get; set; }
+        public string IdentityClientSecret { get; set; }
+        public string IdentityResponseType { get; set; }
+    }
+}
---
+++ src/Application/MoneyTime.Application.WebApi/Settings/MainSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace MoneyTime.Application.WebApi.Settings
+{
+    public class MainSettings
+    {
+        public string IdentityUrl { get; set; }
+        public string IdentityAudience { get; set; }
+        public string IdentityClientSecret { get; set; }
+        public string IdentityResponseType { get; set; }
+
+    }
+}

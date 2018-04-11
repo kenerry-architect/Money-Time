f6310ccb8cec6a28896ad375c75e30eaa43d4ad3
Kenerry Pierry Paulo Serain <kserain.development@gmail.com>
2018-04-10 23:31:39 -0300

    Added swagger

--- Application/MoneyTime.Application.WebApi.Scheduling/Startup.cs
+++ Application/MoneyTime.Application.WebApi.Scheduling/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MoneyTime.Application.WebApi.Settings;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MoneyTime.Application.WebApi.Scheduling
 {
@@ -37,6 +38,31 @@ namespace MoneyTime.Application.WebApi.Scheduling
                     options.ResponseType = settings.IdentityResponseType;
                 });
 
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1", new Info
+                {
+                    Title = "Scheduling API",
+                    Version = "v1",
+                    Description = "Scheduling API service",
+                });
+
+                //options.AddSecurityDefinition("oauth2", new OAuth2Scheme
+                //{
+                //    Type = "oauth2",
+                //    Flow = "application",
+                //    AuthorizationUrl = $"{settings.IdentityUrlExternal}/connect/authorize",
+                //    TokenUrl = $"{settings.IdentityUrlExternal}/connect/token",
+                //    Scopes = new Dictionary<string, string>
+                //    {
+                //        {settings.IdentityAudience, "Profile API"}
+                //    }
+                //});
+
+                //options.OperationFilter<AuthorizeCheckOperationFilter>();
+            });
+
             services.AddMvc();
         }
 
@@ -44,6 +70,15 @@ namespace MoneyTime.Application.WebApi.Scheduling
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Scheduling.WebApi V1");
+                    //c.OAuthClientId("root");
+                    //c.OAuthClientSecret("secret");
+                    c.OAuthAppName("Profile Swagger UI");
+                });
 
             app.UseAuthentication();
             app.UseMvcWithDefaultRoute();
